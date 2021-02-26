c881e97787610be4f91f431e25ac2ecfb776b3a5
Joe Crocker <Joecrocker27@gmail.com>
2021-02-26 09:35:42 +0000

    fixed text capitalisation navbar weirdness

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import styles from './layout.css'
+import Styles from './layout.css'
 
 export const Layout = ({children}) => {
     return (
